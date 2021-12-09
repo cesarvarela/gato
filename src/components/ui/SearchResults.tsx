e4b5ba4d6b06638b9a4f5be68c0dda72ccccba04
Cesar Varela <mail@cesarvarela.com>
2021-12-09 02:48:07 -0300

    Open list of items using arrow keys and command enter

--- src/components/ui/SearchResults.tsx
+++ src/components/ui/SearchResults.tsx
@@ -1,6 +1,51 @@
-import React from 'react'
+import Mousetrap from 'mousetrap'
+import React, { useCallback, useEffect, useState } from 'react'
+import { classnames } from 'tailwindcss-classnames'
 
 function SearchResults({ results }) {
+
+    const [selectedIndex, setSelectedIndex] = useState(0)
+
+    useEffect(() => {
+
+        setSelectedIndex(0)
+
+    }, [results])
+
+
+    const up = useCallback(() => {
+
+        setSelectedIndex(index => index > 0 ? index - 1 : results.length - 1)
+
+    }, [results])
+
+    const down = useCallback(() => {
+
+        setSelectedIndex(index => index < results.length - 2 ? index + 1 : 0)
+
+    }, [results])
+
+    const open = useCallback(() => {
+
+        const result = results[selectedIndex]
+
+        window.open(result.link)
+
+    }, [results, selectedIndex])
+
+
+    useEffect(() => {
+
+        Mousetrap.bind('up', up)
+        Mousetrap.bind('down', down)
+        Mousetrap.bind('command+enter', open)
+
+        return () => {
+
+            Mousetrap.unbind(['up', 'down', 'command+enter'])
+        }
+
+    }, [Mousetrap, selectedIndex, results])
 
     if (results.length === 0) {
 
@@ -16,15 +61,15 @@ function SearchResults({ results }) {
 
     return <table className="min-w-full divide-y divide-gray-200">
         <tbody className="bg-white divide-y divide-gray-200">
-            {results.map(result =>
-                <tr>
+            {results.map((result, index) =>
+                <tr key={result.link} className={classnames({ "bg-gray-300": index == selectedIndex })} >
                     <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                             <div className="ml-4">
                                 <div className="text-sm font-medium text-gray-900">
                                     {result.title}
                                 </div>
-                                <div className="text-sm text-gray-500" dangerouslySetInnerHTML={{ __html: result.htmlFormattedUrl }} />
+                                <a href={result.link} className="text-sm text-gray-500" dangerouslySetInnerHTML={{ __html: result.htmlFormattedUrl }} />
                             </div>
                         </div>
                     </td>
