96801a65de19aa5dd598f7e61c5d5e6ce055942d
Cesar Varela <mail@cesarvarela.com>
2021-12-22 17:32:04 -0300

    Kind of improve web persona suggestions

--- src/main/Web.ts
+++ src/main/Web.ts
@@ -2,6 +2,10 @@ import { IParseResult, IPersona, PersonaName } from '../interfaces';
 import isURL from 'validator/lib/isURL';
 import Reader from './Reader';
 
+const fixHTTP = href => href.startsWith('http')
+    ? href
+    : href.startsWith('localhost') ? `http://${href}` : `https://${href}`
+
 class Web implements IPersona {
 
     name: PersonaName = 'web'
@@ -26,18 +30,39 @@ class Web implements IPersona {
 
     async parse(q: string): Promise<IParseResult> {
 
-        if (isURL(q, { require_protocol: false })
-            || isURL(q, { require_tld: false, require_protocol: false, host_whitelist: ['localhost'] })
-        ) {
+        const isSomeURL = isURL(q, { require_protocol: false, require_tld: false, require_port: false })
 
-            const whitelisted = await this.reader.isWhitelisted({ url: q })
+        if (isSomeURL) {
 
-            const href = q.startsWith('http')
-                ? q
-                : q.startsWith('localhost') ? `http://${q}` : `https://${q}`
+            const hasProtocol = isURL(q, { require_protocol: true, require_tld: false, require_port: false })
+            const hasTLD = isURL(q, { require_protocol: false, require_tld: true, require_port: false })
+            const hasPort = isURL(q, { require_protocol: false, require_tld: false, require_port: true })
 
-            return { name: this.name, confidence: whitelisted ? 7 : 5, href }
+            let href = q
+            let confidence = 5
+
+            if (await this.reader.isWhitelisted({ url: q })) {
+                confidence++
+            }
+
+            if (hasProtocol) {
+                confidence++
+            }
+
+            if (hasTLD) {
+                confidence++
+            }
+
+            if (hasPort) {
+                confidence++
+            }
+
+            href = fixHTTP(href)
+
+            return { name: this.name, confidence, href }
         }
+
+        return { name: this.name, confidence: 0, href: q }
     }
 }
 
